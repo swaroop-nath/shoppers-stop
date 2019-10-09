6fc2a1e5ce2c6924e72c137080b830aed5c1eea0
swaroop-nath <swaroopnath6@gmail.com>
2019-10-10 00:54:04 +0530

    Initial Commit. The project architecture has been created to integrate Angular with Spring Boot.

---
+++ shoppers-stop-backend/src/main/java/org/ecommerce/shoppersstopbackend/ShoppersStopApplication.java
@@ -0,0 +1,11 @@
+package org.ecommerce.shoppersstopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersStopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppersStopApplication.class, args);
+    }
+}
---
+++ shoppers-stop-backend/src/main/java/org/ecommerce/shoppersstopbackend/ShoppersStopBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ecommerce.shoppersstopbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppersStopBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppersStopBackendApplication.class, args);
+    }
+
+}
