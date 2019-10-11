1d9051cce53c1a05300ee4666ba740baf963f3a0
swaroop-nath <swaroopnath6@gmail.com>
2019-10-11 23:58:22 +0530

    Entities have been defined for the web-app. Test class has been initialized for testing addition of data to the database.

--- shoppers-stop-frontend/src/app/app.module.ts
+++ shoppers-stop-frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { FilterComponent } from './filter/filter.component';
+import { ProductDescComponent } from './product-desc/product-desc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    FilterComponent,
+    ProductDescComponent
   ],
   imports: [
     BrowserModule
